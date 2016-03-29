585b16a466fede92d4ec86672abc8e23d4275142
ZeroLiShu <lishufox@icloud.com>
2016-03-29 21:33:01 +0800

    add registration

--- django_zhaji_project/settings.py
+++ django_zhaji_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'zhaji.apps.ZhajiConfig',
+    'registration',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -115,6 +116,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+REGISTRATION_OPEN = True        # If True, users can register
+ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
+REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
+LOGIN_REDIRECT_URL = '/zhaji/'  # The page you want users to arrive at after they successful log in
+LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in,
+                                # and are trying to access pages requiring authentication
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
--- django_zhaji_project/urls.py
+++ django_zhaji_project/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^zhaji/', include('zhaji.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
